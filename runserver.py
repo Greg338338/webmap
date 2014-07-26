e1a52b5a5f917c3e334f24852660058d2d8edbd3
Denis <carriere.denis@gmail.com>
2014-07-26 13:37:41 -0400

    Publish to Port 80

--- runserver.py
+++ runserver.py
@@ -2,4 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from webmap import app
-app.run(debug=True)
+import sys
+
+if sys.argv[-1] == 'publish':
+    app.run(host='0.0.0.0', port=80)
+else:
+    app.run(debug=True)
